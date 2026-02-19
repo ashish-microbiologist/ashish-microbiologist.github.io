a47bea8060ec5fd04c1435eaaf163ca724eaf629
Ashish Pratap <opashishytff@gmail.com>
2026-02-19 06:49:20 +0530

    Add files via upload

---
+++ Facebookpro2/src/utils/colors.js
@@ -0,0 +1,37 @@
+export const COLORS = {
+  primary: '#1877F2',
+  secondary: '#42B72A',
+  background: '#FFFFFF',
+  text: '#000000',
+  gray: '#F0F2F5',
+  darkGray: '#65676B',
+  whatsapp: '#25D366',
+  instagram: '#E4405F',
+  telegram: '#0088CC',
+  website: '#4285F4',
+  youtube: '#FF0000',
+  facebook: '#1877F2',
+  messenger: '#0084FF',
+  twitter: '#1DA1F2',
+  danger: '#dc3545',
+  success: '#28a745',
+  warning: '#ffc107',
+  info: '#17a2b8'
+};
+
+export const THEME = {
+  light: {
+    background: '#FFFFFF',
+    text: '#000000',
+    card: '#F0F2F5',
+    border: '#DDDDDD',
+    icon: '#65676B'
+  },
+  dark: {
+    background: '#18191A',
+    text: '#E4E6EB',
+    card: '#242526',
+    border: '#3E4042',
+    icon: '#B0B3B8'
+  }
+};
